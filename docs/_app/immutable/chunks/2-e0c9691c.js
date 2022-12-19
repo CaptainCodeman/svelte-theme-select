1d34c78d6d5cb0675cb8c6aa2ff3febc23ab6878
Simon Green <simon@captaincodeman.com>
2022-12-19 15:52:21 -0700

    tweak colors for demo

---
+++ docs/_app/immutable/chunks/2-e0c9691c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-5d16c7ed.js";export{t as component};
--- docs/_app/immutable/chunks/2-e619896d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-cae726f8.js";export{t as component};
