98cef7a02d763e962f13b4addec48ca4c3fae20f
Simon Green <simon@captaincodeman.com>
2022-12-29 10:01:22 -0700

    update example

---
+++ docs/_app/immutable/chunks/2-206bdacc.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-be80ee7c.js";export{t as component};
--- docs/_app/immutable/chunks/2-e0c9691c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-5d16c7ed.js";export{t as component};
