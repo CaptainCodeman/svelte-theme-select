f5dd975b85fefe27df91b8f79354bbe8df1ae8e6
Simon Green <simon@captaincodeman.com>
2022-12-30 12:14:43 -0700

    bump version

---
+++ docs/_app/immutable/chunks/2-1249fc19.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-037be0e7.js";export{t as component};
--- docs/_app/immutable/chunks/2-206bdacc.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-be80ee7c.js";export{t as component};
